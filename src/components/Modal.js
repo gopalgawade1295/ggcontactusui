1cfac4e11c10709ae86af10729a2fa3ea3b5a1ea
Gopal Gawade <gawadegopal1295@gmail.com>
2024-07-18 18:59:18 +0530

    first commit

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../assets/styles/whatsapp.css";
 import { FaXmark } from "react-icons/fa6";
+import ScrollToBottom from 'react-scroll-to-bottom';
+import { FaArrowRight } from "react-icons/fa6";
 
 const Modal = ({ show, setShow }) => {
     return (
         <div>
             {show ? (
-                <div className="modal" onClick={() => setShow(!show)}>
+                <div className="modal">
                     <div className="modal-div">
                         <div className="close-btn">
                             <FaXmark
@@ -15,7 +17,25 @@ const Modal = ({ show, setShow }) => {
                             />
                         </div>
 
-                        <h3>Future Container</h3>
+                        <ScrollToBottom className="MessageContainer">
+                            <div className="Messages">
+                                <div className={'received'}>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                </div>
+
+                                <div className={'sent'}>
+                                    <p>Hello</p>
+                                </div>
+                            </div>
+                        </ScrollToBottom>
+
+                        <div className="sendmsg">
+                            <input type="text" className="chatinput" placeholder="Type here" />
+
+                            <button className="sendbtn">
+                                    <FaArrowRight size={28} />
+                            </button>
+                        </div>
                     </div>
                 </div>
             ) : null}
